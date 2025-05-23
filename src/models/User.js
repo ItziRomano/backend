97dab6fe3df147a4f868f1b632d455a1a8968a3b
ItziRomano <itzi.romanolo@gmail.com>
2025-05-22 21:09:20 -0600

    fixed

--- src/models/User.js
+++ src/models/User.js
@@ -2,14 +2,14 @@ const pool = require('../config/db');
 const bcrypt = require('bcryptjs');
 
 class User {
-  static async create({ email, password, firstName, lastName }) {
-    const hashedPassword = await bcrypt.hash(password, 10);
+  static async create({ email, password_hash, firstName, lastName }) {
+    // const hashedPassword = await bcrypt.hash(password, 10);
     const query = `
       INSERT INTO users (email, password_hash, first_name, last_name)
       VALUES ($1, $2, $3, $4)
       RETURNING id, email, first_name, last_name, created_at
     `;
-    const values = [email, hashedPassword, firstName, lastName];
+    const values = [email, password_hash, firstName, lastName];
     const { rows } = await pool.query(query, values);
     return rows[0];
   }
